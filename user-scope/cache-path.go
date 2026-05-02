182be36ba64cece83fdd2aee06d435383d41ceba
Max Romanov <maksipapa999@gmail.com>
2026-05-02 12:45:02 +0300

    refactor(user-scope): add YAML marshaling for cache path (master)
    - Implement CachePath.UnmarshalYAML using NewCachePath
    - Implement CachePath.MarshalYAML with user cache dir handling
    - Import yaml.v3 and strings for YAML support and path trimming

--- user-scope/cache-path.go
+++ user-scope/cache-path.go
@@ -4,6 +4,9 @@ import (
 	"fmt"
 	"os"
 	"path/filepath"
+	"strings"
+
+	"gopkg.in/yaml.v3"
 )
 
 const AppName = "smart-pc"
@@ -29,4 +32,31 @@ func (p *CachePath) SetValue(s string) error {
 
 	*p = cp
 	return nil
+}
+
+func (p *CachePath) UnmarshalYAML(value *yaml.Node) error {
+	const op = "user-scope.UnmarshalYAML"
+
+	var str string
+	if err := value.Decode(&str); err != nil {
+		return fmt.Errorf("%s: failed to decode value to string: %w", op, err)
+	}
+	cp, err := NewCachePath(str)
+	if err != nil {
+		return fmt.Errorf("%s: failed to create new cache path: %w", op, err)
+	}
+	*p = cp
+
+	return nil
+}
+
+func (p *CachePath) MarshalYAML() (interface{}, error) {
+	const op = "user-scope.MarshalYAML"
+
+	ucd, err := os.UserCacheDir()
+	if err != nil {
+		return "", fmt.Errorf("%s: can not get user cache dir: %w", op, err)
+	}
+
+	return strings.TrimPrefix(ucd, filepath.Join(ucd, AppName, "")), nil
 }
