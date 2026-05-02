5cff140056b0079a2b23f1bd8e05c72f6f010202
Max Romanov <maksipapa999@gmail.com>
2026-05-02 15:53:23 +0300

    refactor(commands): relax log topic validation with func (master)
    - Allow StartListenOptions to accept LogTopicFunc without LogTopic
    - Update validation to require either log topic or log topic func

--- commands/start-listen-options.go
+++ commands/start-listen-options.go
@@ -25,8 +25,8 @@ func (o *StartListenOptions) check() error {
 	if o.CommandMessageType == "" {
 		errs = append(errs, errors.New("command message type required"))
 	}
-	if o.LogTopic == "" {
-		errs = append(errs, errors.New("log topic type required"))
+	if o.LogTopic == "" && o.LogTopicFunc == nil {
+		errs = append(errs, errors.New("log topic or log topic func is required"))
 	}
 	if o.LogMessageType == "" {
 		errs = append(errs, errors.New("log message type required"))
